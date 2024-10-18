1796c9b5f9c318c49cad26b45cdcf05bed0c85f1
awsabdulhamed <awsabd666@gmail.com>
2024-10-18 22:10:02 +0200

    Initial commit

---
+++ backend/src/service/index.ts
@@ -0,0 +1,14 @@
+import { SquidProject } from '@squidcloud/backend';
+
+/*****************************
+ *                           *
+ *    INTERNAL USE ONLY      *
+ *    DO NOT MODIFY          *
+ *                           *
+ *****************************/
+
+
+
+class ExportedSquidProject extends SquidProject {}
+
+export default new ExportedSquidProject();
